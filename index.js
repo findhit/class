bd37ec6370929f8571141c048cd489147b3866af
José Moreira <jmoreira@microdual.com>
2014-10-06 23:59:19 +0100

    refactoring + associations feature: phase 1

--- index.js
+++ index.js
@@ -2,4 +2,28 @@
   The entry point.
   @module Class
 **/
-module.exports = require("./lib/class")
+
+/**
+ * To use it, you just need to import it:
+ *
+ * ```js
+ * var Class = require('findhit-class');
+ * ```
+ *
+ *
+ * @class Class
+ */
+
+var Class = function () {};
+
+// Mixin extras
+require( './lib/statics' )( Class );
+require( './lib/util' )( Class );
+
+// Mixin features
+require( './lib/features/extendable' )( Class );
+require( './lib/features/hookable' )( Class );
+require( './lib/features/associable' )( Class );
+require( './lib/features/optionable' )( Class );
+
+module.exports = Class;
